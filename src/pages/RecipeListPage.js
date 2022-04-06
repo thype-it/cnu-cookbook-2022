f35d93433a6aab6e60717fc4586421da382dfdfe
thype-it <illit.michal@gmail.com>
2022-04-06 21:06:25 +0200

    loading recipes from api

--- src/pages/RecipeListPage.js
+++ src/pages/RecipeListPage.js
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+import { api } from "../api";
+
 export function RecipeListPage() {
-  return <h1>Recepty</h1>;
+
+  //states
+  const [recipes, setRecipes] = useState();
+
+  //load recipes from API
+  useEffect( () => {
+    api.get('/recipes')
+    .then( (response) => setRecipes(response.data))
+  }, []);
+
+  return (
+    <>
+      <h1>Recepty</h1>
+      {console.log(recipes)}
+    </>
+  )
 }
