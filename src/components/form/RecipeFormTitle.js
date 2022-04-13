e791caaeebd91196a7c3959ad74fb8613aaf4141
thype-it <illit.michal@gmail.com>
2022-04-13 17:09:27 +0200

    added input template for title -recipe

--- src/components/form/RecipeFormTitle.js
+++ src/components/form/RecipeFormTitle.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import { Controller } from 'react-hook-form'
+import { FormFeedback, FormGroup, FormText, Input, Label } from 'reactstrap'
 
-export const RecipeFormTitle = () => {
+export const RecipeFormTitle = ({control, errors}) => {
   return (
-    <div>RecipeFormTitle</div>
+    <FormGroup>
+      <Label for='recipeFormTitle'>
+        Názov *
+      </Label>
+      <Controller
+        name='title'
+        control={control}
+        rules = {{
+          required: 'Prosím zadajte názov.',
+          minLength: {
+            value: 3,
+            message: "Minimálny počet znakov je 3."
+          }
+        }}
+        render={({
+          field,
+          fieldState:{ invalid, isTouched},
+        }) =>
+          <Input
+            id='recipeFormTitle'
+            placeholder='povinný údaj'
+            {...field}
+            invalid={invalid}
+            valid={!invalid && isTouched}
+          />}
+        />
+      <FormFeedback>
+        {errors.title?.message}
+      </FormFeedback>
+    </FormGroup>
   )
 }
--- src/components/general/RecipeForm.js
+++ src/components/general/RecipeForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Form, FormFeedback, FormGroup, FormText, Input, Label, Row } from 'reactstrap';
+import { Col, Form, FormFeedback, FormGroup, FormText, Input, Row } from 'reactstrap';
 import { useForm, Controller, FormProvider } from 'react-hook-form';
 
 //components
@@ -31,70 +31,20 @@ export const RecipeForm = () => {
   }
 
   return (
-    <>
-      <FormProvider>
-        <Form>
-          <RecipeFormTitle/>
-          <Row>
-            <Col>
-              <RecipeFormBasics/>
-            </Col>
-            <Col>
-              <RecipeFormIngredients/>
-            </Col>
-            <Col>
-              <RecipeFormDirections/>
-            </Col>
-          </Row>
-        </Form>
-
-      </FormProvider>
-
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup>
-          <Input
-            plaintext
-            readOnly
-            value="Some plain text/ static value"
-          />
-        </FormGroup>
-        <FormGroup>
-          <Controller
-            name='title'
-            control={control}
-            rules = {{
-              required: 'Prosím zadajte názov.',
-              minLength: {
-                value: 3,
-                message: "Minimálny počet znakov je 3."
-              }
-            }}
-            render={({
-              field,
-              fieldState:{ invalid, isTouched},
-            }) =>
-              <Input
-                {...field}
-                invalid={invalid}
-                valid={!invalid && isTouched}
-              />}
-            />
-          <FormFeedback>
-            {errors.title?.message}
-          </FormFeedback>
-
-          <FormText>daco daco</FormText>
-        </FormGroup>
-        <FormGroup>
-          <Controller
-            name='sideDish'
-            control={control}
-            render={({ field, formState }) => <Input {...field}/>}
-          />
-          <input type='submit'/>
-        </FormGroup>
-      </Form>
-
-    </>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <RecipeFormTitle control={control} errors={errors}/>
+      <Row>
+        <Col>
+          <RecipeFormBasics/>
+        </Col>
+        <Col>
+          <RecipeFormIngredients/>
+        </Col>
+        <Col>
+          <RecipeFormDirections/>
+        </Col>
+      </Row>
+        <input type='submit'/>
+    </Form>
   )
 }
