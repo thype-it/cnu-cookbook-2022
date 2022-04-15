28dda1258934ef238795cc8416553e7e43e47135
thype-it <illit.michal@gmail.com>
2022-04-15 12:10:08 +0200

    implementented ingredients form -inprogress

--- src/components/form/RecipeFormIngredients.js
+++ src/components/form/RecipeFormIngredients.js
@@ -1,7 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Controller, useFormState } from 'react-hook-form';
+import { Col, Form, FormFeedback, FormGroup, FormText, Input, InputGroup, InputGroupText, Label, Row } from 'reactstrap';
 
-export const RecipeFormIngredients = () => {
+export const RecipeFormIngredients = ({control}) => {
+
+  const { dirtyFields } = useFormState({control})
+
   return (
-    <div>RecipeFormIngredients</div>
+    <>
+      <Row>
+        <Col>
+          <FormGroup>
+            <Controller
+              name='amount'
+              control={control}
+              render={({field})=>
+              <Input
+                {...field}
+                type='number'
+                min='0'
+                placeholder='Množstvo'
+                form='formIngredients'
+              />}
+            />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Controller
+              name='amountUnit'
+              control={control}
+              render={({field})=>
+              <Input
+                {...field}
+                placeholder='Jednotka'
+                form='formIngredients'
+              />}
+            />
+          </FormGroup>
+        </Col>
+      </Row>
+      <FormGroup>
+        <InputGroup>
+          <Controller
+            name='name'
+            control={control}
+            render={({field})=>
+            <Input
+              {...field}
+              placeholder='Názov'
+              form='formIngredients'
+            />}
+          />
+          <InputGroupText>
+            <input
+              type='submit'
+              form='formIngredients'
+              value='pridaj'
+              disabled={!dirtyFields.name}
+            />
+          </InputGroupText>
+        </InputGroup>
+      </FormGroup>
+    </>
   )
 }
--- src/components/general/RecipeForm.js
+++ src/components/general/RecipeForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Form, FormFeedback, FormGroup, FormText, Input, Row } from 'reactstrap';
 import { useForm } from 'react-hook-form';
 import { api } from '../../api';
@@ -14,6 +14,9 @@ export const RecipeForm = ({onTitleInput, titleInput}) => {
 
   const navigate = useNavigate();
 
+  const [ingredients, setIngredients] = useState([{}]);
+
+  //registering main form
   const { handleSubmit, control, formState: { errors } } = useForm({
     defaultValues: {
       title: '', //required field for user
@@ -22,48 +25,73 @@ export const RecipeForm = ({onTitleInput, titleInput}) => {
       sideDish: '',
       directions: '',
       ingredients: [{
-          amount: null,
+          amount: '',
           amountUnit: '',
-          isGroup: null,
+          isGroup: false,
           name: '',
       }]
     }
   });
 
+  //registering ingredients form
+  const { handleSubmit: handleSubmitIng, control: controlIng, reset} = useForm({
+    defaultValues: {
+      amount: '',
+      amountUnit: '',
+      name: ''
+    }
+  })
+
+  //create new recipe on main form submit
   const onSubmit = (data) => {
     console.log(data);
-    api.post('/recipes', data)
-    .then((response) => {
-      navigate(
-        `/recipe/${response.data.slug}`,
-        {state:{alert:'newRecipe'}
-      })
-    })
-    .catch((error) => console.log(error))
+    // api.post('/recipes', data)
+    // .then((response) => {
+    //   navigate(
+    //     `/recipe/${response.data.slug}`,
+    //     {state:{alert:'newRecipe'}
+    //   })
+    // })
+    // .catch((error) => console.log(error))
   }
 
+  //add new ingredient obejct to data on ingredients form submit
+  const submitIngredients = (data) => {
+    console.log(data);
+
+    setIngredients(ingredients => ingredients.concat(data))
+
+    console.log(ingredients);
+    reset();
+  }
+
+  //forms template
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <RecipeFormTitle
-        control={control}
-        errors={errors}
-        onTitleInput={onTitleInput}
-        titleInput={titleInput}
-      />
-      <Row>
-        <Col>
-          <h4>Základné údaje</h4>
-          <RecipeFormBasics  control={control} errors={errors}/>
-        </Col>
-        <Col>
-          <RecipeFormIngredients/>
-        </Col>
-        <Col>
-          <h4>Postup</h4>
-          <RecipeFormDirections control={control}/>
-        </Col>
-      </Row>
-        <input type='submit'/>
-    </Form>
+    <>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <RecipeFormTitle
+          control={control}
+          errors={errors}
+          onTitleInput={onTitleInput}
+          titleInput={titleInput}
+        />
+        <Row>
+          <Col>
+            <h4>Základné údaje</h4>
+            <RecipeFormBasics  control={control} errors={errors}/>
+          </Col>
+          <Col>
+            <h4>Ingrediencie</h4>
+            <RecipeFormIngredients control={controlIng} reset={reset}/>
+          </Col>
+          <Col>
+            <h4>Postup</h4>
+            <RecipeFormDirections control={control}/>
+          </Col>
+        </Row>
+          <input type='submit'/>
+      </Form>
+      <Form id='formIngredients' onSubmit={handleSubmitIng(submitIngredients)}></Form>
+    </>
   )
 }
