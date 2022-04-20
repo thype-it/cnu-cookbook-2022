ea570778a681bb5fe6b350926888490ae8035b83
thype-it <illit.michal@gmail.com>
2022-04-20 23:30:52 +0200

    added button form navigating home

--- src/components/helpers/Error.js
+++ src/components/helpers/Error.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button } from 'reactstrap'
 
 export const Error = ({message}) => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>{message}</div>
+    <>
+      <div>{message}</div>
+      <Button onClick={()=> navigate('/')}>Naspäť domov</Button>
+    </>
   )
 }
