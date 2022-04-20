bfeee81e57fcd78366a899c460b676608a54aaab
thype-it <illit.michal@gmail.com>
2022-04-20 23:28:02 +0200

    added sorting with prep time

--- src/components/helpers/SearchRecipes.js
+++ src/components/helpers/SearchRecipes.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Col, Input, Label, Row } from 'reactstrap';
+import { Button, ButtonGroup, Col, Input, Label, Row } from 'reactstrap';
 import { api } from '../../api';
 import { RecipeListItem } from '../../pages/children/RecipeListItem';
 
 export const SearchRecipes = () => {
 
   const [recipes, setRecipes] = useState([]);
+  const [filtered, setFiltered] = useState([]);
   const [query, setQuery] = useState('');
   const [isSearch, setSearch] = useState(false);
 
@@ -19,21 +20,47 @@ export const SearchRecipes = () => {
   const handleSearch = (e) => {
     setSearch(true);
     setQuery(e.target.value);
+    setFiltered(
+      recipes.filter(recipe =>
+        recipe.title.toLowerCase().includes(query.toLowerCase())
+      )
+    )
   };
 
-  const filteredRecpies = recipes.filter(recipe =>
-    recipe.title.toLowerCase().includes(query.toLowerCase())
-  );
+
+
+  const filterUnder = (minutes) => {
+    setSearch(true);
+    setFiltered(
+      recipes.filter(recipe =>
+        recipe.preparationTime <= minutes
+      )
+    )
+  }
 
   return (
     <>
+    <Label>Čas prípravy do</Label>
+    <Row>
+      <ButtonGroup>
+        <Button onClick={()=>filterUnder(15)}>
+          15m
+        </Button>
+        <Button onClick={()=>filterUnder(30)}>
+          30m
+        </Button>
+        <Button onClick={()=>filterUnder(60)}>
+          1h
+        </Button>
+      </ButtonGroup>
+    </Row>
       <Label>Názov receptu</Label>
       <Input
         className='mb-2'
         onChange={handleSearch}
       />
       <Row className='mt-4'>
-        {filteredRecpies.map((recipe, i) =>
+        {filtered.map((recipe, i) =>
           <Col key={recipe._id} xs='6'className='mb-3'>
             <Link
               style={{textDecoration:'none'}}
